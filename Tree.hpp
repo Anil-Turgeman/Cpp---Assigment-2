8170c2b03d45451522bc2192f501a1beaebd17f2
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-03-26 17:17:54 +0200

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -1,25 +1,39 @@
 namespace ariel{
-    struct node {
-                int data;
-                node* left;     //less then the data
-                node* right;    //greater then the data
-            };
-            
     class Tree{
+
         private:
-            node* rootValue;;
-            
+            struct node{
+                int data;
+                node* left;
+                node* right;
+            };
+
+            node* rootValue;
+
+            void   insertPrivate(int data, node* nodeCheck);
+            void   removePrivate(int data, node* parent);
+            void   removeRoot();
+            void   removeMatch(node* parent, node* match, bool left);
+            int    sizePrivate(node* ptr);
+            bool   containsPrivate(int data, node* ptr);
+            node*  returnNodePrivate(int data, node* ptr);
+            int    parentPrivate(int data, node* ptr);
+            void   printPrivate(node* ptr);
+            int    findSmallestPrivate(node* ptr);
 
         public:
             Tree();
-            void insert(int i);
-            void remove(int i);
-            int size();
-            bool contains(int i);
-            int root();
-            int parent(int i);
-            int left(int i);
-            int right(int i);
-            void print();
-    };
+            node*  createLeaf(int i);
+            void   insert(int i);
+            void   remove(int i);
+            int    size();
+            bool   contains(int i);
+            int    root();
+            int    parent(int i);
+            int    right(int i);
+            int    left(int i);
+            void   print();
+            node*  returnNode(int i);//help function
+            int    findSmallest();//help function
+        };
 };
