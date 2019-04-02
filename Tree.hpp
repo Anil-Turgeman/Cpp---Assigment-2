1c361d2bdad8ebee74b8fd850da2227dcee6d352
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-02 16:33:19 +0300

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -1,38 +1,42 @@
 #pragma once
 namespace ariel{
-class Node{
-public:
-  int _data;
-  Node* left;
-  Node* right;
-  Node(int data);
-  int getData();
-};
-class Tree{
-public:
-  Node* _root;
-private:
-  int _size;
+  class Node{
+    private:
+      int _data;
+      Node* left;
+      Node* right;
+    public:
+      Node(int data); //constructor
+      Node* getLeft(); 
+      Node* getRight(); 
+      void setLeft(Node* left);
+      void setRight(Node* right);
+      int getData();
+  };
 
-public:
-//Constructor & Destructor
-  Tree();
-  ~Tree();
-//Methods
-  Node* find(int i);
-  Tree& insert(int i);
-  void remove(int i);
-  int size();
-  bool contains(int i);
-  int root();
-  int parent(int i);
-  int left(int i);
-  int right(int i);
-  void print();
-private:
-  void printRecursive(Node* curr);
-  Node* find(Node* curr,int i);
-  void Destroy(Node* curr);
-  Node& getRoot();
-};
+  class Tree{
+    private:
+      int _size;
+      Node* _root;
+    public:
+    //Constructor & Destructor
+      Tree();
+      ~Tree();
+    //Methods
+      Tree& insert(int i);
+      void remove(int i);
+      int size();
+      bool contains(int i);
+      int root();
+      int parent(int i); //returns the parent of node conatining i.
+      int left(int i); //returns the data inside the left child of node containing i.
+      int right(int i); //returns the data inside the right child of node containing i.
+      void print(); //prints the tree in-order.
+      Node* getRoot(); //getter for the root.
+    private:
+      Node* find(int i); //finds i inside the tree
+      void printRecursive(Node* curr);
+      void Destroy(Node* curr);
+      
+  };
 }
