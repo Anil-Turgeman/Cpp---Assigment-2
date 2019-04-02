a06b8d1345ce10227d18c65d90bc1965f976a6c8
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-02 16:18:12 +0300

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -1,41 +1,34 @@
-namespace ariel{
-    class Tree{
+#include <iostream>
+using namespace std;
 
-        private:
-            struct node{
-                int data;
-                node* left;
-                node* right;
-            };
-
-            node* rootValue;
-
-            void   insertPrivate(int data, node* nodeCheck);
-            void   removePrivate(int data, node* parent);
-            void   removeRoot();
-            void   removeMatch(node* parent, node* match, bool left);
-            int    sizePrivate(node* ptr);
-            bool   containsPrivate(int data, node* ptr);
-            node*  SearchNodePrivate(int data, node* ptr);
-            int    parentPrivate(int data, node* ptr);
-            void   printPrivate(node* ptr);
-            int    findSmallestPrivate(node* ptr);
-            void   DeconstructorTree(node* nodeCheck);
-
-        public:
-            Tree();
-            ~Tree();
-            node*  createLeaf(int i);
-            void   insert(int i);
-            void   remove(int i);
-            int    size();
-            bool   contains(int i);
-            int    root();
-            int    parent(int i);
-            int    right(int i);
-            int    left(int i);
-            void   print();
-            node*  SearchNode(int i);//help function
-            int    findSmallest();//help function
-        };
+struct Node
+{
+	int data;
+	Node* left;
+	Node* right;
 };
+
+
+namespace ariel{
+
+	class Tree{
+	public:
+	Node* myroot;
+
+	Tree();
+	void insert(int);
+	void print();
+	int parent (int x);
+	int right(int x);
+	int left(int x);
+	int size();
+	int root();
+	bool contains(int x);
+	bool remove(int x);
+	~Tree();
+	};
+}
+
+
+
+
