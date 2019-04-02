a9444c9f18bcddcdcf7d9da40dadb413ce472ac3
Anil Turgeman <34038554+Anil-Turgeman@users.noreply.github.com>
2019-04-02 16:18:57 +0300

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -6,29 +6,39 @@ struct Node
 	int data;
 	Node* left;
 	Node* right;
+	Node* parent;
+
+	Node(int x);
+	// ~Node();
 };
 
+namespace ariel
+{
 
-namespace ariel{
+class Tree
+{
+  private:
+  int tree_size;
+	void helpInsert(Node* myNode, Node* node,Node* parent);
+	void helpPrint(Node* node);
+	Node* catch_num(Node* node, int x);
+	bool helpContains(Node* node,int x);
 
-	class Tree{
-	public:
+
+  public:
 	Node* myroot;
 
 	Tree();
+	void clear(Node* node);
 	void insert(int);
 	void print();
-	int parent (int x);
+	int parent(int x);
 	int right(int x);
 	int left(int x);
 	int size();
 	int root();
 	bool contains(int x);
-	bool remove(int x);
+	void remove(int x);
 	~Tree();
-	};
-}
-
-
-
-
+};
+} // namespace ariel
